fd60f52d662e41d498762a34d205f74016c9927a
tomato2718 <qweasdzxc030@gmail.com>
2024-03-20 17:34:11 +0800

    Feat: 建立 Card

--- showdown/showdown/__main__.py
+++ showdown/showdown/__main__.py
@@ -1,5 +1,10 @@
+from ._game import Game
+from ._card import CardStack
+
 def main():
-    ...
+    card_stack = CardStack()
+    game = Game(card_stack=card_stack)
+    game.start()
 
 if __name__ == "__main__":
     main()
